117e84fd73cc107a253e46978d07f106b8ab32c2
Artenis Molla <artenismolla@MacBook-Pro-di-Artenis.local>
2021-12-26 21:28:07 +0100

    tile menu

--- lib/widgets/home/menu_tile.dart
+++ lib/widgets/home/menu_tile.dart
@@ -26,26 +26,35 @@ class MenuTile extends StatelessWidget {
             },
         child: Row(
           children: [
-            ClipRRect(
-              child: Container(
-                  height: mediaQuery.size.height * 0.15,
-                  width: mediaQuery.size.width * 0.30,
-                  margin: EdgeInsets.only(top: 20, bottom: 0, left: 20),
+            Container(
+                height: mediaQuery.size.height * 0.20,
+                width: mediaQuery.size.width * 0.30,
+                margin: EdgeInsets.only(top: 20, bottom: 0, left: 20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomLeft: Radius.circular(20)),
                   child: Image.network(
                     menu.linkUrl,
                     fit: BoxFit.cover,
                   ),
-                  decoration: BoxDecoration(
+                ),
+                decoration: BoxDecoration(
                     color: UIColors.violet,
-                  )),
-            ),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20)))),
             Container(
-                height: mediaQuery.size.height * 0.15,
+                height: mediaQuery.size.height * 0.20,
                 width: mediaQuery.size.width * 0.57,
                 margin: EdgeInsets.only(top: 20, bottom: 0, left: 10),
-                decoration: BoxDecoration(color: UIColors.detailBlack),
+                decoration: BoxDecoration(
+                    color: UIColors.detailBlack,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20))),
                 child: Padding(
-                  padding: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(20.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
@@ -75,7 +84,7 @@ class MenuTile extends StatelessWidget {
                         children: [
                           Expanded(
                             child: Text(
-                              "---- Descrizione da sistemare ------",
+                              "Descrizione da sistemare",
                               style: TextStyle(
                                   color: UIColors.white.withOpacity(0.8),
                                   fontSize: 15),
